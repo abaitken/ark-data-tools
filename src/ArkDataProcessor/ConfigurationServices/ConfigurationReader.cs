94491540e5982a2896ddd50398fec6b346f966ce
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-09-03 18:01:52 +0100

    feat: Implemented functionality to split out monitoring source configuration

--- src/ArkDataProcessor/ConfigurationServices/ConfigurationReader.cs
+++ src/ArkDataProcessor/ConfigurationServices/ConfigurationReader.cs
@@ -12,7 +12,45 @@ namespace ArkDataProcessor
             var result = serializer.Deserialize(reader, typeof(Configuration)) as Configuration;
             if (result == null)
                 throw new InvalidOperationException("Failed to load configuration");
+
+            if(result.MonitoringSources != null && result.MonitoringSources.Count != 0)
+            {
+                foreach (var item in result.MonitoringSources)
+                    LoadAndMerge(configurationFile, item);
+            }
+
             return result;
+        }
+
+        private void LoadAndMerge(string parentConfigurationFile, MonitoringSource item)
+        {
+            if (string.IsNullOrEmpty(item.ExternalConfiguration))
+                return;
+
+            var configurationFile = ResolveFilePath(item.ExternalConfiguration, parentConfigurationFile);
+
+            var serializer = new JsonSerializer();
+            using var reader = new StreamReader(configurationFile);
+
+            var external = serializer.Deserialize(reader, typeof(MonitoringSource)) as MonitoringSource;
+            if (external == null)
+                throw new InvalidOperationException("Failed to load monitoring source");
+            item.FilePath = external.FilePath ?? item.FilePath;
+            item.Filters = external.Filters ?? item.Filters;
+            item.UploadTargets = external.UploadTargets ?? item.UploadTargets;
+        }
+
+        private string ResolveFilePath(string filePath, string relativeTo)
+        {
+            if (Path.IsPathRooted(filePath))
+                return filePath;
+
+            var fullPath = Path.GetFullPath(relativeTo);
+            var directory = Path.GetDirectoryName(fullPath);
+            var subjectPath = Path.Combine(directory ?? "." , filePath);
+            var resolvedPath = Path.GetFullPath(subjectPath);
+
+            return resolvedPath;
         }
     }
 }
--- src/ArkDataProcessor/Model/MonitoringSource.cs
+++ src/ArkDataProcessor/Model/MonitoringSource.cs
@@ -4,6 +4,7 @@ namespace ArkDataProcessor
 {
     public class MonitoringSource
     {
+        public string ExternalConfiguration { get; set; }
         public string FilePath { get; set; }
         public List<UploadTarget> UploadTargets { get; set; }
         public List<Filter> Filters { get; set; }
