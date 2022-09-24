0c86592daaee40056d36cd529216bab6035ed555
Alex Boyne-Aitken <abaitken@live.co.uk>
2022-09-24 17:41:43 +0100

    fix: Fixed crash when copying local file when folder does not exist

--- src/ArkDataProcessor/PipelineTasks/LocalCopyFilePipelineTask.cs
+++ src/ArkDataProcessor/PipelineTasks/LocalCopyFilePipelineTask.cs
@@ -5,7 +5,12 @@
         internal override Task Execute(IEnumerable<UploadItem> uploadItems, UploadTarget uploadTarget)
         {
             foreach (var item in uploadItems)
+            {
+                var directoryName = Path.GetDirectoryName(item.RemotePath);
+                if (!string.IsNullOrWhiteSpace(directoryName) && !Directory.Exists(directoryName))
+                    Directory.CreateDirectory(directoryName);
                 File.Copy(item.LocalPath, item.RemotePath, true);
+            }
             return Task.CompletedTask;
         }
     }
